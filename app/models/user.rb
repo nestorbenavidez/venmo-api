7a893c44050221fdde721d93461225789e36e448
Nestor R Benavidez <nestor.benavidez@gmail.com>
2019-02-06 12:47:12 -0300

    Create transaction in payment to encapsulate actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   has_friendship
 
   def payment(friend_id,amount,description = "NA")
-    self.balance = self.balance.to_f - amount.to_f
-    self.save!
-    friend = User.find_by(friend_id)
-    friend.balance = balance.to_f +  amount.to_f
-    friend.save!
-    t = Transaction.create(user_to: friend_id, user_from: id, amount: amount, description: description)
+    ActiveRecord::Base.transaction do
+      self.balance = self.balance.to_f - amount.to_f
+      self.save!
+      friend = User.find_by(friend_id)
+      friend.balance = balance.to_f +  amount.to_f
+      friend.save!
+      t = Transaction.create(user_to: friend_id, user_from: id, amount: amount, description: description)
+    end
   end
 end
